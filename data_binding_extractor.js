6c081955f40c02922f2d9cb0b5f58c1351a55ed3
kongliangzhong <kongliangzhong@gmail.com>
2018-06-25 16:53:25 +0800

    update data_binding_extractor.js

--- data_binding_extractor.js
+++ data_binding_extractor.js
@@ -1,18 +1,21 @@
 const request = require("request-promise");
 const fs = require("fs");
 const Web3 = require("web3");
-const web3 = new Web3(new Web3.providers.HttpProvider("https://api.myetherapi.com/eth"));
+const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/hM4sFGiBdqbnGTxk5YT2"));
 const lrcAbi = '[{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"bonusPercentages","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"DECIMALS","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"BLOCKS_PER_PHASE","outputs":[{"name":"","type":"uint16"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"MAX_UNSOLD_RATIO","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"HARD_CAP","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"BASE_RATE","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"close","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"saleStarted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"issueIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"recipient","type":"address"}],"name":"issueToken","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"_firstblock","type":"uint256"}],"name":"start","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"hardCapReached","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"saleEnded","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"unsoldTokenIssued","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"price","outputs":[{"name":"tokens","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"GOAL","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"NAME","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalEthReceived","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"saleDue","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"target","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"NUM_OF_PHASE","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"firstblock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"SYMBOL","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"inputs":[{"name":"_target","type":"address"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[],"name":"SaleStarted","type":"event"},{"anonymous":false,"inputs":[],"name":"SaleEnded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"caller","type":"address"}],"name":"InvalidCaller","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"msg","type":"bytes"}],"name":"InvalidState","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"issueIndex","type":"uint256"},{"indexed":false,"name":"addr","type":"address"},{"indexed":false,"name":"ethAmount","type":"uint256"},{"indexed":false,"name":"tokenAmount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[],"name":"SaleSucceeded","type":"event"},{"anonymous":false,"inputs":[],"name":"SaleFailed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]'; // tslint:disable-line
 
 const lrcAddr = "0xEF68e7C694F40c8202821eDF525dE3782458639f";
 const lrcToken = new web3.eth.Contract(JSON.parse(lrcAbi), lrcAddr);
 
 const targetProjectId = "1";
+let resultFile = "eth-neo-binding.csv";
+const lrcHoldersFile = "lrc-holders";
+const bindingStartBlock = 5104000;
+const DEST_BLOCK = 5850000;
+
 const bindingMap = new Map();
 const invalidBindingMap = new Map();
 const lrcBalanceMap = new Map();
-const resultFile = "eth-neo-binding.csv";
-const lrcHoldersFile = "lrc-holders";
 
 function parseBindingPairs(response) {
   const result = [];
@@ -78,25 +81,40 @@ async function getAllLrcHolderBalances() {
   const keys = [...bindingMap.keys()];
   for (const key of keys) {
     console.log("query balance for address:", key);
-    const balance = await lrcToken.methods.balanceOf(key).call();
+    const balance = await lrcToken.methods.balanceOf(key).call(DEST_BLOCK);
     const balanceInt = Math.floor(balance/1e18);
-    if (balanceInt === 0) {
-      console.log("lrc balance is 0, skip.");
-    } else {
-      lrcBalanceMap.set(key, balanceInt);
-    }
+    lrcBalanceMap.set(key, balanceInt);
   }
 }
 
 function writeResultToFile() {
   const keys = [...bindingMap.keys()];
-  keys.forEach((key) => {
-    fs.appendFileSync(resultFile, key + "," + bindingMap.get(key) + "," +
-                      lrcBalanceMap.get(key) + "\n");
+  const secs = Math.floor(new Date().getTime() / 1000);
+  resultFile = resultFile + "." + secs;
+
+  const resultTupple = keys.map((key) => [key, bindingMap.get(key), lrcBalanceMap.get(key)]);
+  const sorter = (a, b) => {
+    if (a[2] >= b[2]) return -1;
+    else return 1;
+  };
+  resultTupple.sort(sorter);
+
+  resultTupple.forEach((item) => {
+    fs.appendFileSync(resultFile, item[0] + "," + item[1] + "," + item[2] + "\n");
   });
 }
 
 async function main() {
+  var args = process.argv.slice(2);
+
+  let fromBlock = bindingStartBlock;
+  if (args && args.length > 0) {
+    const newFromBlock = args[0];
+    if (newFromBlock > fromBlock) {
+      fromBlock = newFromBlock;
+    }
+  }
+
   await crawlBindings();
   await getAllLrcHolderBalances();
   writeResultToFile();
